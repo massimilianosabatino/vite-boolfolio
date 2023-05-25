26f444621cc348a708935619dbd06869c58e3488
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-25 22:50:56 +0200

    fix: cards grid layout

--- src/components/MainAppComponent.vue
+++ src/components/MainAppComponent.vue
@@ -12,9 +12,11 @@ export default {
 </script>
 
 <template>
-    <div class="row">
-        <div class="col-md-3" v-for="project in store.projects">
-            <CardSingle :project="project" />
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class="col" v-for="project in store.projects">
+                <CardSingle :project="project" />
+            </div>
         </div>
     </div>
 </template>
