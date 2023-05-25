fcf4d64ef596a804d05340ef3b69449cbac62b08
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-25 22:42:59 +0200

    add: source on img and fix project type and tech

--- src/components/partials/CardSingle.vue
+++ src/components/partials/CardSingle.vue
@@ -14,12 +14,12 @@ export default {
 <template>
     <div>
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img :src="project.cover" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ project.title }}</h5>
                 <p class="card-text">{{ project.description }}</p>
-                <div>Type: {{ project.type.category }}</div>
-                <div>Technologies:
+                <div>Type: {{ project.type ? project.type.category : 'Nessuna categoria' }}</div>
+                <div v-if="project.technologies.length">Technologies:
                     <template v-for="(technology, index) in project.technologies">
                         <span v-if="index < project.technologies.length - 1">{{ technology.technology }} - </span>
                         <span v-else>{{ technology.technology }}</span>
