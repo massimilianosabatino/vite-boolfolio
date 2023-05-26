c1fecbb3a1be389498d98baa77ba038c0391ccfe
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-26 16:24:09 +0200

    add: nav to header

--- src/components/HeaderAppComponent.vue
+++ src/components/HeaderAppComponent.vue
@@ -1,11 +1,39 @@
 <script>
-    export default {}
+export default {
+    name: "AppHeader",
+}
 </script>
 
 <template>
-
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Massimiliano</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">
+                                Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'projects' }" class="nav-link">
+                                Progetti
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contatti</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
