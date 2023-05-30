4c3e62cff903408c1b99c0998d0e95123f6c8cfe
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-30 23:02:32 +0200

    feat: filter by technology

--- src/components/ProjectsListComponent.vue
+++ src/components/ProjectsListComponent.vue
@@ -8,7 +8,9 @@ export default {
             store,
             projects: [],
             types: null,
-            category: null
+            categorySelected: null,
+            technologies: null,
+            technologySelected: null
             
         };
     },
@@ -33,20 +35,36 @@ export default {
                 .catch((error) => {
                     console.log(error);
                 })
+        },
+        //Get all categories
+        getTechnologies(){
+            axios.get(this.store.apiBaseUrl + this.store.technologiesApi)
+                .then((response) => {
+                    this.technologies = response.data.results;
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
         }
     },
     created() {
         //Launch API call when the component is created
         this.getProject();
         this.getTypes();
+        this.getTechnologies();
     },
     computed: {
         projectList(){
-            if(this.category){
-                return this.projects.filter(project => project.type.category === this.category);
+            if(this.categorySelected){
+                return this.projects.filter(project => project.type.category === this.categorySelected);
+            }
+            if(this.technologySelected){
+                console.log(this.projects)
+                return this.projects.filter(project => project.technologies.find(({technology}) => technology === this.technologySelected));
             }
             return this.projects
-        }
+        },
+        
     }
 }
 </script>
@@ -65,8 +83,22 @@ export default {
                     </button>
                     <ul class="dropdown-menu" v-if="types">
                         <li :key="type.id" v-for="type in types">
-                        <a class="dropdown-item" href="#" @click.prevent="category = type.category">{{ type.category }}</a>
+                        <a class="dropdown-item" href="#" @click.prevent="categorySelected = type.category">{{ type.category }}</a>
                         </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto">
+                <div class="dropdown">
+                    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Technologies
+                    </button>
+                    <ul class="dropdown-menu" v-if="technologies">
+                        <li :key="technology.id" v-for="technology in technologies">
+                        <a class="dropdown-item" href="#" @click.prevent="technologySelected = technology.technology">{{ technology.technology }}</a>
+                        </li>
+                        <li><a class="dropdown-item" href="#" @click.prevent="technologySelected = null">reset</a></li>
                     </ul>
                 </div>
             </div>
