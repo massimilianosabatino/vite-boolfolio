0f5d4ee506b3c01eb66288010ad421b2954b1862
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-05 21:00:48 +0200

    add: basic home page

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -8,7 +8,14 @@ export default {
 </script>
 <template>
 <div class="home">
-    Home
+    <div class="container h-100">
+        <div id="name" class="row text-center align-items-center h-100">
+            <div class="col">
+                <h1>MASSIMILIANO SABATINO</h1>
+                <h2>Tecnologo della comunicazione audiovisiva e multimediale</h2>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 <style>
