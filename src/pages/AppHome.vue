283af93c26cc502563e6937a5aeaa1fb58abd5ce
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-06-05 23:58:04 +0200

    add: link to projects on home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,9 +10,15 @@ export default {
 <div class="home">
     <div class="container h-100">
         <div id="name" class="row text-center align-items-center h-100">
-            <div class="col">
+            <div class="col-12">
                 <h1>MASSIMILIANO SABATINO</h1>
                 <h2>Tecnologo della comunicazione audiovisiva e multimediale</h2>
+                <div class="link-projects">
+                    <router-link :to="{ name: 'projects' }"
+                        class="align-self-center my-4">
+                        Portfolio
+                    </router-link>
+                </div>
             </div>
         </div>
     </div>
